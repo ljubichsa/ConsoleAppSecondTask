fe949a6b6583e374317b5edb52383a491901b3a1
ljubichsa <ljubich.stas@gmail.com>
2016-05-25 20:21:31 +0600

    Version 1.0

---
+++ ConsoleAppSecondTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSecondTask
+{
+    class Program
+    {
+        static void GetNumbers(IEnumerable<int> collection, int number)
+        {
+            foreach (var item in collection.SelectMany(x => collection
+                                                            .Where(y => x + y == number)
+                                                            .Select(y => new { x = x, y = y })))
+                Console.WriteLine(item);
+        } 
+
+        static void Main(string[] args)
+        {
+            GetNumbers(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 }, 6);
+            GetNumbers(new List<int>() { -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8 }, 6);
+            GetNumbers(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8 }, 6);
+            Console.Read();
+        }
+    }
+}
